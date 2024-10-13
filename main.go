111fc780250f12e37fc23af410f28eac14679c86
bandari-abhilash <bandariabhilash0@gmail.com>
2024-10-14 02:55:49 +0530

    to check interfaces across mutliple os

--- main.go
+++ main.go
@@ -6,14 +6,22 @@ import (
 	"os"
 )
 
+type interfaces struct {
+	index         int
+	interfaceName string
+	interfaceIp   string
+	multicast     net.Flags
+}
+
 func main() {
+	var interfacesLists []interfaces
 	var options string
 	fmt.Println("----Select from the following options-----")
 	fmt.Println("1. Multicast Sender(Will initiate multicast data)")
 	fmt.Println("2. Multicast Receiver(Will receive Multicast Data)")
 	fmt.Println("3. Exit")
 
-	fmt.Scan(&options)
+	// fmt.Scan(&options)
 
 	switch options {
 	case "1":
@@ -27,10 +35,45 @@ func main() {
 	}
 
 	interfaceList, err := net.Interfaces()
+	val := net.IPv6interfacelocalallnodes
+
+	fmt.Println(val)
 	if err != nil {
 		fmt.Println("Error while listing Interfaces")
 	}
+	var count int = 0
+	for _, iface := range interfaceList {
+		if iface.Flags&net.FlagUp != 0&net.FlagBroadcast&net.FlagMulticast {
+			count += 1
+			tempObj := interfaces{
+				index:         iface.Index,
+				interfaceName: iface.Name,
+				interfaceIp:   iface.HardwareAddr.String(),
+				multicast:     net.FlagMulticast,
+			}
+			addrs, errA := iface.Addrs()
+			if errA != nil {
+				fmt.Println("Error:", errA)
+			}
+			for _, addr := range addrs {
+				if _, ok := addr.(*net.IPNet); ok {
+					fmt.Println("-------------", addr.String())
+					// fmt.Printf("%-40s %s (%s)\n", ipNet.IP.String(), iface.Name, iface.HardwareAddr)
+				}
+			}
+			interfacesLists = append(interfacesLists, tempObj)
+			// fmt.Println("----------------", tempObj)
+			// fmt.Printf("The interface name %s and the broadcast flag %d   \n", iface.Name, net.FlagBroadcast)
+		}
+		// fmt.Println("------------", interfacesLists)
+		// fmt.Println("Interface name", iface)
+		// fmt.Println("hardware address ------------------", iface.HardwareAddr.String())
 
-	fmt.Printf("The interfaces on this system are %v", interfaceList)
+	}
+
+	fmt.Println("Count is", count)
+	// fmt.Println(interfacesLists)
+
+	// fmt.Printf("The interfaces on this system are %v", interfaceList)
 
 }
